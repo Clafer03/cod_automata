5f5ced204b0b8d35b03f4e1d0256b86628ebd085
Clafer03 <claudi125.lol@gmail.com>
2023-09-22 19:59:50 -0500

    Automata

---
+++ automata.cpp
@@ -0,0 +1,42 @@
+/*
+Programa para representar cualquier automata
+*/
+
+#include <iostream>
+using namespace std;
+#define GetSize(array_enteros) (sizeof(array_enteros)/sizeof(*(array_enteros)))
+int main()
+{
+	string c1 = "1110010";
+	int alfabeto[] = {0,1};	
+	int transicion[] = {0,1,2};
+	int a = GetSize(alfabeto);
+	int h = GetSize(transicion);
+	int matriz[a][h] = {
+						{2,0},
+						{1,1},
+						{2,1}
+	                   };
+
+    /**
+     * f|0|1
+     * 0|2|0
+     * 1|1|1
+     * 2|2|1
+     * 
+     * 
+    */
+    for(int i=0; i<c1.length();i++)
+    {
+        if(c1[i]=='1')
+        {
+            for(int i = 0; i < a; i++)
+            {
+                for(int j = 0; j < h; j++)
+                {
+                    
+                }
+            }
+        }
+    }
+}
