c4a250903693642584df9fa4284be31046fca9a5
Clafer03 <claudi125.lol@gmail.com>
2023-09-29 19:02:08 -0500

    AutomataToken1

--- automata_Token.cpp
+++ automata_Token.cpp
@@ -5,13 +5,14 @@ using namespace std;
 
 int main()
 {
-	string c1;
+    string c1;
     int n, num , tam, trans;
-    int eI, eF, eN = 0;
-	int alfabeto[] = {0,1};	
-	int transicion[] = {0,1,2};
+    int eI=0, eN = 0;
+    char alfabeto2[] = {'l','d','.','o'};
+	int transicion[] = {0,1,2,3,4,5,6,7};
+    int eF[] = {2,4,7};
 	int a = GetSize(transicion);
-	int h = GetSize(alfabeto);
+	int h = GetSize(alfabeto2);
 	int matriz[a][h]; 
 
     /**
@@ -20,7 +21,7 @@ int main()
      * ingreso de estados I y F     ✓
      * ingreso de cadena            ✓
     */
-
+/*
     //Ingreso de alfabeto------------------------
     cout << "Ingrese el tamaño del alfabeto: ";
     cin >> tam;
@@ -70,7 +71,7 @@ int main()
     }
 
     cout << endl;
-
+*/
     //Ingreso de cadena
     cout << "Cadena : "; 
     cin >> c1;
@@ -81,10 +82,10 @@ int main()
     {
         if(c1[i]=='1'||c1[i]=='0')
         {
-            for (int j = 0; j < GetSize(alfabeto); j++)
+            for (int j = 0; j < GetSize(alfabeto2); j++)
             {
                 num = c1[i] - '0'; //convierte el caracter a num ---  1110010
-                if (num == alfabeto[j]) 
+                if (num == alfabeto2[j]) 
                 {
                     eN = j;
                 }
@@ -121,11 +122,49 @@ int main()
 	{2,1}
 */
 /**
-    * f|0     |1
-    * 0|2(0,0)|0(0,1)
-    * 1|1(1,0)|1(1,1)
-    * 2|2(2,0)|1(2,1)
+    * f|l     |d     |o     |.
+    * 0|1(0,0)|3(0,1)|-(0,2)|-(0,3)
+    * 1|1(1,0)|1(1,1)|2(1,2)|-(1,3)
+    * 2|-(2,0)|-(2,1)|-(2,2)|-(2,3)
+    * 3|-(3,0)|3(3,1)|4(3,2)|5(3,3)
+    * 4|-(4,0)|-(4,1)|-(4,2)|-(4,3)
+    * 5|-(5,0)|6(5,1)|-(5,2)|-(5,3)
+    * 6|-(6,0)|6(6,1)|7(6,2)|-(6,3)
+    * 7|-(7,0)|-(7,1)|-(7,2)|-(7,3)
     * 
-    * 1110010
+    * 33.3 => <REAL>
+    * 
+    * 
+    *   vector<int> num;
+        vector<char> car;
+        
+        string c1 = "ll34";
+        for(int i = 0; i< c1.length(); i++)
+        {
+            cout << c1[i] - '0' << " ";
+            for (char c : c1) {
+            if (isdigit(c)) {
+                num.push_back(c);
+            } else if (isalpha(c)) {
+                car.push_back(c);
+            }
+        }
+        }
+        cout << endl;
+        //numero en ASCII
+        for(int i = 0; i< 2; i++)
+        {
+            cout << num[i] - '0' << " ";
+        }
+        cout << endl;
+        
+        //cadena normal
+        for(int i = 0; i< 2; i++)
+        {
+            cout << car[i] << " ";
+        }
+    * 
+    * 
+    * 
 */
 
